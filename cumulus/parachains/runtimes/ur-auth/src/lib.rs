288d7b8db65cf16ed8bb6a37f410547514183995
Cute_Wisp <sweatpotato13@gmail.com>
2023-12-11 12:39:01 +0900

    refactor: 💡 fix is_para to is_relay

--- cumulus/parachains/runtimes/ur-auth/src/lib.rs
+++ cumulus/parachains/runtimes/ur-auth/src/lib.rs
@@ -412,14 +412,14 @@ impl pallet_asset_link::Config for Runtime {
 
 parameter_types! {
 	pub const AggregatedPeriod: BlockNumber = 10;
-	pub const IsPara: bool = true;
+	pub const IsRelay: bool = false;
 }
 
 impl system_token_aggregator::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type Period = AggregatedPeriod;
 	type AssetMultiLocationGetter = AssetLink;
-	type IsPara = IsPara;
+	type IsRelay = IsRelay;
 }
 
 parameter_types! {
--- infrablockspace/runtime/parachains/src/system_token_aggregator.rs
+++ infrablockspace/runtime/parachains/src/system_token_aggregator.rs
@@ -60,7 +60,7 @@ pub mod pallet {
 		#[pallet::constant]
 		type Period: Get<BlockNumberFor<Self>>;
 		type AssetMultiLocationGetter: AssetMultiLocationGetter<AssetId>;
-		type IsPara: Get<bool>;
+		type IsRelay: Get<bool>;
 	}
 
 	#[pallet::event]
@@ -87,8 +87,8 @@ pub mod pallet {
 	{
 		fn on_initialize(n: BlockNumberFor<T>) -> Weight {
 			if n % T::Period::get() == Zero::zero() {
-				let is_para = T::IsPara::get();
-				Self::do_aggregate_system_token(is_para);
+				let is_relay = T::IsRelay::get();
+				Self::do_aggregate_system_token(is_relay);
 				T::DbWeight::get().reads(3)
 			} else {
 				T::DbWeight::get().reads(0)
@@ -98,7 +98,7 @@ pub mod pallet {
 }
 
 impl<T: Config> Pallet<T> {
-	pub(crate) fn do_aggregate_system_token(is_para: bool)
+	pub(crate) fn do_aggregate_system_token(is_relay: bool)
 	where
 		u32: From<BlockNumberFor<T>>,
 		<<T as frame_system::Config>::RuntimeOrigin as OriginTrait>::AccountId:
@@ -124,7 +124,7 @@ impl<T: Config> Pallet<T> {
 					fee_account.clone(),
 					amount,
 					asset_multilocation.clone(),
-					is_para,
+					is_relay,
 				);
 
 				Self::deposit_event(Event::<T>::SystemTokenAggregated {
