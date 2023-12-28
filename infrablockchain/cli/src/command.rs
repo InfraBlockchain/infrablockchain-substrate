25e2924323eb7bdb24f64d9fbcc63716653c4059
energyGiver <ego.abraxas@gmail.com>
2023-12-28 15:26:40 +0900

    hotfix: change repo name

--- infrablockchain/cli/src/command.rs
+++ infrablockchain/cli/src/command.rs
@@ -62,7 +62,7 @@ impl SubstrateCli for Cli {
 	}
 
 	fn support_url() -> String {
-		"https://github.com/InfraBlockchain/infrablockchain-sdk/issues/new".into()
+		"https://github.com/InfraBlockchain/infrablockchain-substrate/issues/new".into()
 	}
 
 	fn copyright_start_year() -> i32 {
